c818d5907c5d743c53ae70801a2aefa694f904c4
Melisa <melissaserna19@gmail.com>
2024-07-09 16:23:34 -0500

    Se organiza sección fúturo en su tamaño

---
+++ js/futuro.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Animación de entrada para el contenido principal
+    const mainContent = document.querySelector('.main-futuro');
+    mainContent.style.opacity = '1';
+
+      // Animación de entrada para la imagen
+    const imgFuturo = document.querySelector('.img-futuro');
+    imgFuturo.style.opacity = '1';
+});
