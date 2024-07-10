8aaf4ee0ff89140c154d55376e5242c62f281269
Tatiana Romero <tamarome@gmail.com>
2024-07-10 14:24:19 -0500

    se agrega carrusel y estilos

---
+++ js/oportunidades.js
@@ -0,0 +1,19 @@
+// Carrusel Tatiana*
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+// fin Carrusel Tatiana
